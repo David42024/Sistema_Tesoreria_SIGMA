8be28dca98dcc4bebab0b310b3d31f3c13306492
RobertoA1 <duckyroberto@gmail.com>
2026-01-14 20:38:51 -0500

    fix(backend): Corregido un problema que utilizaba un método inexistente.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -67,7 +67,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (!App::runningInConsole() && !App::runningInConsoleCommand('migrate')) {
+        if (!App::runningInConsole()) {
             $this->noConsoleBoot();
         }
 
