9e373bda5f7d09bcabb6b91692b7848211e9eb70
David42024 <daln486279513@gmail.com>
2026-02-13 18:05:15 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,21 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('alpinejs')) return 'vendor-alpine';
+                        if (id.includes('apexcharts')) return 'vendor-apexcharts';
+                        if (id.includes('sweetalert2')) return 'vendor-sweetalert';
+                        if (id.includes('flatpickr')) return 'vendor-flatpickr';
+                        if (id.includes('jsvectormap')) return 'vendor-jsvectormap';
+                        if (id.includes('dropzone')) return 'vendor-dropzone';
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 });
