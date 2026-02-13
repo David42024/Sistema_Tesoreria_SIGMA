bd16f3c9060becdd8f55f82c16c35ff5cb10093a
David42024 <daln486279513@gmail.com>
2026-02-13 18:20:29 -0500

    cambios realizados para css

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css', 
+                'resources/css/style.css',
                 'resources/js/index.js',
                 'resources/css/login.css',
             ],
